766f05f9171481dd0691ae7824e102585ef49629
sumit parise <sumitparise49@gmail.com>
2025-03-02 23:53:44 +0530

    add auth-serivice

---
+++ Mango.Web/Service/IService/IAuthService.cs
@@ -0,0 +1,11 @@
+﻿using Mango.Web.Models;
+
+namespace Mango.Web.Service.IService
+{
+	public interface IAuthService
+	{
+		Task<ResponseDto> LoginAsync(LoginRequestDto loginRequestDto);
+		Task<ResponseDto> RegistrationAsync(LoginRequestDto loginRequestDto);
+		Task<ResponseDto> AssignRoleAsync(LoginRequestDto loginRequestDto);
+	}
+}
--- Mango.Web/obj/Debug/net8.0/Mango.Web.AssemblyInfo.cs
+++ Mango.Web/obj/Debug/net8.0/Mango.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mango.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3fbc33f1995424df6e41e1d9ce19b6500f05972d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d953afa305270ddec1e43ae8e3c212761aa9f1f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mango.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mango.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
