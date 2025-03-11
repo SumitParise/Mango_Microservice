dd5ac4d011cbbc293bb3af6521ba8e44fd68b3e2
sumit parise <sumitparise49@gmail.com>
2025-03-12 00:21:05 +0530

    add cookies

--- Mango.Web/Controllers/AuthController.cs
+++ Mango.Web/Controllers/AuthController.cs
@@ -1,9 +1,13 @@
 ﻿using Mango.Web.Models;
 using Mango.Web.Service.IService;
 using Mango.Web.Utility;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Newtonsoft.Json;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace Mango.Web.Controllers
@@ -34,6 +38,7 @@ namespace Mango.Web.Controllers
 			{
 				LoginResponseDto loginResponse = JsonConvert.DeserializeObject<LoginResponseDto>(Convert.ToString(response.Result));
 
+				await SignInUser(loginResponse);
 				_tokenProvider.SetToken(loginResponse.Token);
 
 
@@ -98,6 +103,36 @@ namespace Mango.Web.Controllers
 		public IActionResult Logout()
 		{
 			return View();
+		}
+
+		public async Task SignInUser(LoginResponseDto model)
+		{
+			var handler = new JwtSecurityTokenHandler(); 
+			var jwt = handler.ReadJwtToken(model.Token);  // get token
+
+			var identity = new ClaimsIdentity(CookieAuthenticationDefaults.AuthenticationScheme);
+
+			//Extracting all claims
+
+			identity.AddClaim(new Claim(JwtRegisteredClaimNames.Email, 
+				jwt.Claims.FirstOrDefault(u => u.Type == JwtRegisteredClaimNames.Email).Value));
+
+			identity.AddClaim(new Claim(JwtRegisteredClaimNames.Sub,
+				jwt.Claims.FirstOrDefault(u => u.Type == JwtRegisteredClaimNames.Sub).Value));
+
+			identity.AddClaim(new Claim(JwtRegisteredClaimNames.Name,
+				jwt.Claims.FirstOrDefault(u => u.Type == JwtRegisteredClaimNames.Name).Value));
+
+			identity.AddClaim(new Claim(ClaimTypes.Name,
+			jwt.Claims.FirstOrDefault(u => u.Type == JwtRegisteredClaimNames.Email).Value));
+
+			//set principal
+
+			var principal = new ClaimsPrincipal(identity);
+
+			
+
+			await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, principal);
 		}
 	}
 }
--- Mango.Web/obj/Debug/net8.0/Mango.Web.AssemblyInfo.cs
+++ Mango.Web/obj/Debug/net8.0/Mango.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mango.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8c2c9f6f46c7c985f7f0d6c898c3ed0588885af")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36985bf6a3546770c1268b5107d2b96e25c24111")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mango.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mango.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
